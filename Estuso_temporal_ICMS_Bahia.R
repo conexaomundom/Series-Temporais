632aa2bdc1259fd6464aacf5364a1d1816a16c6f
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2019-12-15 09:08:26 -0300

    Análises escritas sobre a tendência, Kelfanio

--- Estuso_temporal_ICMS_Bahia.R
+++ Estuso_temporal_ICMS_Bahia.R
@@ -1,12 +1,6 @@
 rm(list=ls())
 install.packages("forecast")
 
-rmse <- function(y,y_hat){ sqrt(mean((y - y_hat)^2)) }
-rae <- function(y, y_hat){ sum(abs(y_hat - y)) / sum(abs(mean(y) - y)) }
-rrse <- function(y, y_hat){ sum((y_hat - y)^2) / sum((mean(y) - y)^2) }
-mae <- function(y, y_hat){ sum( abs(y - y_hat)) /length(y) }
-
-
 ###
 # Pegando os dados, arrumando as variÃ¡veis
 ficheiro = file.choose()
@@ -17,9 +11,6 @@ attach(icms_bahia)
 igpdi <- icms_bahia$V1
 icms_ba <- icms_bahia$V2
 ####
-
-# Matrizes que vao receber as mÃ©idas das estimativas de rood mean square error
-# a quantidade dessas matrizes 
 
 ###
 # AnÃ¡lise Descritiva Sem considerar a inflaÃ§Ã£o
@@ -44,7 +35,9 @@ icms_d <- icms_ba / igpdi_d
 summary(icms_d)
 boxplot(cbind(icms_ba, icms_d), main = "ICMS mensal, \n Janeiro de 1995 |-| Agosto de 2019, Bahia", ylab = "ICMS")
 ###
-
+# Podemos verificar que a serie deflacionada possui seus valores mais elevados
+# alem de possuir um outlier no mês ___ do ano ___, alem de possuir a media e a 
+# mediana mais elevada assim diminuindo a amplitude da serie.
 ###
 # Colocando o ICMS da Bahia deflacionado em formato de sÃ©rie temporal
 library(tseries)
@@ -52,34 +45,57 @@ icmsba_d.ts = ts(icms_d, start = c(1995,1), frequency = 12)
 # Plot da sÃ©rie e plot do decompose
 plot(icmsba_d.ts)
 plot(decompose(icmsba_d.ts))
+# Vemos no grafico uma clara tendencia de ordem crescente, tendencia essa
+# que se torna bastante visivel apartir dos anos 2000, não demostrando nenhuma
+# grande mudança nesta tendencia desde então, alem disso podemos ver uma sazionalidade
+# no periodo anual e tambem nos meses dos anos, assim apresentando uma clara sazionalidade 
+# em todo o deccorer da serie.
 
 # ACF e PACF
 acf(icmsba_d.ts)
 pacf(icmsba_d.ts)
+# Observa-se que todos os lag do ACF estão fora do intervalo de confiança assim indicando
+# uma possibilidade elevada de não ser representado por um modelo AR, alem disso o PACF
+# após o terceiro lag demonstra uma queda exponencial, se aproximando do zero e se tornando
+# oscilatório após isso indicaria uma possível representação de um modelo MA(2), mas pela existência
+# de sazonalidade e da tendência, está serie possivelmete se encaixaria melhor em um modelo SARIMA,
+# mas como não fomos apresentados para estes modelos não podemos analisar com maiores detalhes esta 
+# possibilidade.
+library(forecast)
+auto.arima(icmsba_d.ts)
+# Pela função auto.arima temos indicios que o melhor modelo para representar esta serie
+# seria o modelo MA(2) com uma diferenciação, e que possui sazionalidade na serie assim sendo
+# o modelo seria um SMA(2)
 
 # Pegando a tendencia para 
 icmsba_d_STL.ts <- stl(icmsba_d.ts, "periodic")
 plot(icmsba_d.ts)
 lines(icmsba_d_STL.ts$time.series[, "trend"], lwd = 1.4, col = "red")
 
+# Verificamos em vermelho a tendencia da serie temporal, precisaremos destes valore para alguns possiveis
+# ajusates para esta serie temporal, alem disso observa-se claramente a tendencia novamente, desta vez
+# ainda mais facilmente por estar sobreposta a propria serie temporal.
+
 ###
 # Teste de estacionaridade
 library(tseries)
 adf.test(icmsba_d.ts, alternative = "stationary")
-###
 
+# Ao nivel de significancia de 5%, ou seja, considerando a probabilidade do erro tipo I,
+# probabilidade de rejeitar H0 dado que H0 é verdadeiro, igual a 5%, temos que a hipótese nula
+# de não estacionaridade deve ser rejeitada, dado que p-valor, que representa a probabilidade de
+# cometer este erro tipo I considerando os dados amostrais é de aproximadamente 0,01.
+# Assim temos que esta serie temporal possui estacionaridade.
+
+
+###
 ####
 
 # PrevisÃ£o 
 library(forecast)
-t0 <- forecast(icmsba_d.ts, h=12, level = c(95,99))
+forecast(icmsba_d.ts, h=12, level = c(95,99))
 plot(forecast(icmsba_d.ts, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
 #Por conta da sazionalidade podemos prever por bastante tempo.  
-
-rmse0 <- RMSE(t0,mat_teste0)
-rae0 <- rae(t0,mat_teste0)
-mae0 <- mae(t0,mat_teste0)
-rrse0 <- rrse(t0,mat_teste0)
 
 ####
 # Primeira forma de retirar a tendÃªncia tomando uma diferenÃ§a
@@ -87,35 +103,40 @@ rrse0 <- rrse(t0,mat_teste0)
 dif_icmsba_d.ts <- diff(icmsba_d.ts)
 plot(dif_icmsba_d.ts)
 plot(decompose(dif_icmsba_d.ts))
+# Vemos no grafico que a tendencia foi removida, assim apresentando aleatoriedade em seus dados
+# observanfo a sazionalidade desta serie, temos que ela permanece proxima a da serie de dados original
+# assim temos que a diferenciação conseguiu remover a tendencia da serie
 
 # ACF e PACF
 acf(dif_icmsba_d.ts)
 pacf(dif_icmsba_d.ts)
-
-# Pegando a tendencia para 
-dif_icmsba_d_STL.ts <- stl(dif_icmsba_d.ts, "periodic")
-plot(dif_icmsba_d.ts)
-lines(dif_icmsba_d_STL.ts$time.series[, "trend"], lwd = 1.4, col = "red")
+auto.arima(dif_icmsba_d.ts)
+# Analisando o ACF e o PACF em conjundo gera-se uma duvida em qual modelo melhor se aplica para esta serie
+# pois o PACF mantem sua queda exponencial apois o terceiro lag, mas o ACF apesar de não ser tão claro demostra
+# possiveis sinais da possibilidade da existencia da um AR na serie, assim gerando duvidas entre um modelo ARMA e um modelo MA
+# lembrando que pela existencia da sazionalidade teriamos que incluir um componente para esta sazionalidade no modelo
+# Como a função auto.arima temos indicios de que o modelo que melhor se aplica para esta serie temporal seria um 
+# MA(2) com o componente se sazionalidade, como foi aplicada uma diferenciação na serie original que tinha como
+# um possivel modelo de melhor aplicação um MA(2) com o componente sazional e uma diferenciação então este resultado
+# seria o esperado.
 
 ###
 # Teste de estacionaridade
 library(tseries)
 adf.test(dif_icmsba_d.ts, alternative = "stationary")
+# Novamente considerando a probabilidade do erro tipo I, probabilidade de rejeitar H0 dado que H0
+# é verdadeiro, igual a 5%, temos que a hipótese nula de não estacionaridade deve ser rejeitada, 
+# dado que p-valor, que representa a probabilidade de cometer este erro tipo I considerando os dados
+# amostrais é de aproximadamente 0,01.
+# Assim temos que esta serie temporal continua a possuir estacionaridade mesmo apos a aplicação de uma diferenciação.
+
 ###
 
 # PrevisÃ£o 
 library(forecast)
-t1 <- forecast(dif_icmsba_d.ts, h=12, level = c(95,99))
+forecast(dif_icmsba_d.ts, h=12, level = c(95,99))
 plot(forecast(dif_icmsba_d.ts, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
-  #Por conta da sazionalidade podemos prever por bastante tempo.  
-rmse1 <- rmse(t1,mat_teste1)
-rae1 <- rae(t1,mat_teste1)
-mae1 <- mae(t1,mat_teste1)
-rrse1 <- rrse(t1,mat_teste1)
-
-####
-
-
+#Por conta da sazionalidade podemos prever por bastante tempo.  
 
 ####
 # Segunda forma de retirar a tendÃªncia taxa de crescimento, log
@@ -123,78 +144,72 @@ rrse1 <- rrse(t1,mat_teste1)
 log_icmsba_d.ts <- log(icmsba_d.ts)
 plot(log_icmsba_d.ts)
 plot(decompose(log_icmsba_d.ts))
+# Vemos no grafico que a tendencia de ordem crescente se mantem, tendencia essa
+# que se mantem bastante visivel apartir dos anos 2000, não demostrando nenhuma
+# grande mudança em comparação com a serie sem a aplicação do log, alem disso podemos
+# observar uma alteração na sazionalidade desta serie, mas mantendo-se sazional
+# assim não possuindo uma alteração significativa nestes aspectos da serie temporal original.
 
 # ACF e PACF
 acf(log_icmsba_d.ts)
 pacf(log_icmsba_d.ts)
+# Novamente observa-se que todos os lag do ACF estão fora do intervalo de confiança indicando
+# uma possibilidade elevada de não ser representado por um modelo AR, alem disso o PACF novamente
+# após o terceiro lag demonstra uma queda exponencial, indicando de forma ainda mais direta uma
+# possível representação de um modelo MA(2), mas por se manter a existência de sazonalidade e da
+# tendência, está serie possivelmete se encaixaria melhor em um modelo SARIMA.
 
-# Pegando a tendencia para 
-log_icmsba_d_STL.ts <- stl(log_icmsba_d.ts, "periodic")
-plot(log_icmsba_d.ts)
-lines(log_icmsba_d_STL.ts$time.series[, "trend"], lwd = 1.4, col = "red")
 
-###
 # Teste de estacionaridade
 library(tseries)
 adf.test(log_icmsba_d.ts, alternative = "stationary")
+
+# Novamente considerando a probabilidade do erro tipo I, probabilidade de rejeitar H0 dado que H0
+# é verdadeiro, igual a 5%, temos que a hipótese nula de não estacionaridade deve ser rejeitada, 
+# dado que p-valor, que representa a probabilidade de cometer este erro tipo I considerando os dados
+# amostrais é de aproximadamente 0,01.
+# Assim temos que esta serie temporal continua a possuir estacionaridade mesmo apos a aplicação do log.
+
+
 ###
+
+####
 
 # PrevisÃ£o 
 library(forecast)
-t2 <- forecast(log_icmsba_d.ts, h=12, level = c(95,99))
-plot(forecast(log_icmsba_d.ts, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
+forecast(icmsba_d.ts, h=12, level = c(95,99))
+plot(forecast(icmsba_d.ts, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
 #Por conta da sazionalidade podemos prever por bastante tempo.  
 
-rmse2 <- RMSE(t2,mat_teste2)
-rae2 <- rae(t2,mat_teste2)
-mae2 <- mae(t2,mat_teste2)
-rrse2 <- rrse(t2,mat_teste2)
-
-####
 
 ####
 # Terceira forma de serie removendo a tendencia
-icms.sem.tend <- icmsba_d.ts - icmsba_d_STL.ts$time.series[, "trend"]
-plot(icms.sem.tend)
 
-plot(decompose(icms.sem.tend))
+icms.sem.tend.ts <- icmsba_d.ts - icmsba_d_STL.ts$time.series[, "trend"]
+plot(icms.sem.tend.ts)
+plot(decompose(icms.sem.tend.ts))
+# Vemos no grafico que a tendencia foi removida, assim apresentando aleatoriedade em seus dados
+# observanfo a sazionalidade desta serie, temos que ela permanece proxima a da serie de dados original
+# mas como o principal objetivo desta transformação é o de remover a tendencia, ele foi visivelmente alcançado.
 
 # ACF e PACF
-acf(icms.sem.tend)
-pacf(icms.sem.tend)
+acf(icms.sem.tend.ts)
+pacf(icms.sem.tend.ts)
+# Apartir do ACF e do PACF não somos capazes de termos uma ideia de qual seria o modelo que 
+# melhor representaria esta serie temporal, sendo assim nescessario uma analise mais aprofundada
+# para poder determinarmos qual seria o modelo que melhor representaria esta serie temporal.
 
-# Pegando a tendencia para 
-icms.sem.tend_STL.ts <- stl(icms.sem.tend, "periodic")
-plot(icms.sem.tend)
-lines(icms.sem.tend_STL.ts$time.series[, "trend"], lwd = 1.4, col = "red")
-
-###
 # Teste de estacionaridade
 library(tseries)
-adf.test(icms.sem.tend, alternative = "stationary")
-###
+adf.test(icms.sem.tend.ts)
+# Novamente considerando a probabilidade do erro tipo I, probabilidade de rejeitar H0 dado que H0
+# é verdadeiro, igual a 5%, temos que a hipótese nula de não estacionaridade deve ser rejeitada, 
+# dado que p-valor, que representa a probabilidade de cometer este erro tipo I considerando os dados
+# amostrais é de aproximadamente 0,01.
+# Assim temos que esta serie temporal continua a possuir estacionaridade mesmo apos remoção de sua tendencia.
 
-# PrevisÃ£o 
+
+
 library(forecast)
-t3 <- forecast(icms.sem.tend, h=12, level = c(95,99))
-plot(forecast(icms.sem.tend, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
-#Por conta da sazionalidade podemos prever por bastante tempo.  
-
-rmse3 <- RMSE(t3,mat_teste3)
-rae3 <- rae(t3,mat_teste3)
-mae3 <- mae(t3,mat_teste3)
-rrse3 <- rrse(t3,mat_teste3)
-
-
-####
-# Avaliando o melhor modelo de previsÃ£o
-
-mRMSE < -c(rmse0, rmse1, rmse2, rmse3)
-mrae <- c(rae0, rae1, rae2, rae3)
-mmae <- c(mae0, mae1, mae2, mae3)
-mrrse <- c(rrse0, rrse1, rrse2, rrse3)
-
-which(mRMSE == min(mRMSE))
-which(mrae == min(mrae))
-which(mmae == min(mmae))
-which(mrrse == min(mmrse))
+forecast(icms.sem.tend.ts, h=12, level = c(95,99))
+plot(forecast(icms.sem.tend.ts, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
