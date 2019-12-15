8a3420c115a81e8308f1a39d7ab5a24ac1ee057a
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2019-12-14 23:32:13 -0300

    Avaliando o melhor série para previsão.
    Avaliando MSE, RRSE, MAE, RAE.

--- Estuso_temporal_ICMS_Bahia.R
+++ Estuso_temporal_ICMS_Bahia.R
@@ -1,6 +1,12 @@
 rm(list=ls())
 install.packages("forecast")
 
+rmse <- function(y,y_hat){ sqrt(mean((y - y_hat)^2)) }
+rae <- function(y, y_hat){ sum(abs(y_hat - y)) / sum(abs(mean(y) - y)) }
+rrse <- function(y, y_hat){ sum((y_hat - y)^2) / sum((mean(y) - y)^2) }
+mae <- function(y, y_hat){ sum( abs(y - y_hat)) /length(y) }
+
+
 ###
 # Pegando os dados, arrumando as variáveis
 ficheiro = file.choose()
@@ -11,6 +17,9 @@ attach(icms_bahia)
 igpdi <- icms_bahia$V1
 icms_ba <- icms_bahia$V2
 ####
+
+# Matrizes que vao receber as méidas das estimativas de rood mean square error
+# a quantidade dessas matrizes 
 
 ###
 # Análise Descritiva Sem considerar a inflação
@@ -63,10 +72,14 @@ adf.test(icmsba_d.ts, alternative = "stationary")
 
 # Previsão 
 library(forecast)
-forecast(icmsba_d.ts, h=12, level = c(95,99))
+t0 <- forecast(icmsba_d.ts, h=12, level = c(95,99))
 plot(forecast(icmsba_d.ts, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
 #Por conta da sazionalidade podemos prever por bastante tempo.  
 
+rmse0 <- RMSE(t0,mat_teste0)
+rae0 <- rae(t0,mat_teste0)
+mae0 <- mae(t0,mat_teste0)
+rrse0 <- rrse(t0,mat_teste0)
 
 ####
 # Primeira forma de retirar a tendência tomando uma diferença
@@ -92,18 +105,15 @@ adf.test(dif_icmsba_d.ts, alternative = "stationary")
 
 # Previsão 
 library(forecast)
-forecast(dif_icmsba_d.ts, h=12, level = c(95,99))
+t1 <- forecast(dif_icmsba_d.ts, h=12, level = c(95,99))
 plot(forecast(dif_icmsba_d.ts, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
-#Por conta da sazionalidade podemos prever por bastante tempo.  
-
+  #Por conta da sazionalidade podemos prever por bastante tempo.  
+rmse1 <- rmse(t1,mat_teste1)
+rae1 <- rae(t1,mat_teste1)
+mae1 <- mae(t1,mat_teste1)
+rrse1 <- rrse(t1,mat_teste1)
 
 ####
-
-# Previsão 
-library(forecast)
-forecast(icmsba_d.ts, h=12, level = c(95,99))
-plot(forecast(icmsba_d.ts, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
-#Por conta da sazionalidade podemos prever por bastante tempo.  
 
 
 
@@ -131,19 +141,16 @@ adf.test(log_icmsba_d.ts, alternative = "stationary")
 
 # Previsão 
 library(forecast)
-forecast(log_icmsba_d.ts, h=12, level = c(95,99))
+t2 <- forecast(log_icmsba_d.ts, h=12, level = c(95,99))
 plot(forecast(log_icmsba_d.ts, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
 #Por conta da sazionalidade podemos prever por bastante tempo.  
 
+rmse2 <- RMSE(t2,mat_teste2)
+rae2 <- rae(t2,mat_teste2)
+mae2 <- mae(t2,mat_teste2)
+rrse2 <- rrse(t2,mat_teste2)
 
 ####
-
-# Previsão 
-library(forecast)
-forecast(icmsba_d.ts, h=12, level = c(95,99))
-plot(forecast(icmsba_d.ts, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
-#Por conta da sazionalidade podemos prever por bastante tempo.  
-
 
 ####
 # Terceira forma de serie removendo a tendencia
@@ -169,6 +176,25 @@ adf.test(icms.sem.tend, alternative = "stationary")
 
 # Previsão 
 library(forecast)
-forecast(icms.sem.tend, h=12, level = c(95,99))
+t3 <- forecast(icms.sem.tend, h=12, level = c(95,99))
 plot(forecast(icms.sem.tend, h=12, level = c(95,99)),shadecols = c( "#9ECAE1", "#6BAED6"), fcol = "darkblue", flwd = 1)
 #Por conta da sazionalidade podemos prever por bastante tempo.  
+
+rmse3 <- RMSE(t3,mat_teste3)
+rae3 <- rae(t3,mat_teste3)
+mae3 <- mae(t3,mat_teste3)
+rrse3 <- rrse(t3,mat_teste3)
+
+
+####
+# Avaliando o melhor modelo de previsão
+
+mRMSE < -c(rmse0, rmse1, rmse2, rmse3)
+mrae <- c(rae0, rae1, rae2, rae3)
+mmae <- c(mae0, mae1, mae2, mae3)
+mrrse <- c(rrse0, rrse1, rrse2, rrse3)
+
+which(mRMSE == min(mRMSE))
+which(mrae == min(mrae))
+which(mmae == min(mmae))
+which(mrrse == min(mmrse))
