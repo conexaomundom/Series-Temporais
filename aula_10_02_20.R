0fa160924cc00ab8e8ca7cb8ebaa3f21256293c7
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2020-02-10 11:46:59 -0300

    Adicionando algumas forma de previsao no R.

--- aula_10_02_20.R
+++ aula_10_02_20.R
@@ -15,8 +15,21 @@ autoplot(AirPassengers)
 summary(AirPassengers)
 AirPassengers
 
-autoplot(AirPassengers, col = "red", linetype = "dashed")
+autoplot(AirPassengers, col = "purple", linetype = 6)
 ?autoplot
+
+# Ajuste e diagnostico
+sarima(AirPassengers, 2,1,1,0,1,0,12)
+
+meanf(AirPassengers, 1)
+naive(AirPassengers, 1)
+summary(AirPassengers, 1)
+snaive(AirPassengers, 1)
+rwf(AirPassengers, 1 ,drift = TRUE)
+
 
 ndiffs(AirPassengers)
 nsdiffs(AirPassengers)
+
+dados <- auto.arima(AirPassengers)
+plot(dados)
