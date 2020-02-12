5ee159a6829e73f0143ba59c7349284bc9d0dbaf
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2020-02-12 11:17:33 -0300

    Metodos de previsão.
    Prevendo e avaliando.

--- aula_10_02_20.R
+++ aula_10_02_20.R
@@ -45,7 +45,37 @@ meanf(AirPassengers, 1)
 naive(AirPassengers, 1)
 summary(AirPassengers, 1)
 snaive(AirPassengers, 1)
+rwf(AirPassengers, 1)
 
 # drift
 rwf(AirPassengers, 1 ,drift = TRUE)
 
+AirPassengers_F <- window(AirPassengers, start = 1949, end = c(1960,12))
+autoplot(AirPassengers_F) +
+  autolayer(meanf(AirPassengers_F, h =11), series = "mean", PI = FALSE) +
+  autolayer(naive(AirPassengers_F, h =11), series = "naive", PI = FALSE) +
+  autolayer(snaive(AirPassengers_F, h =11), series = "Naive Sazonal", PI = FALSE) +
+  autolayer(rwf(AirPassengers_F, h =11, drift = TRUE), series = "Naive com drift", PI = FALSE) +
+  ggtitle("Total mensal de passageiros de companhias aéreas internacionais") +
+  guides(colour = guide_legend(title = "Forecast"))
+
+
+# Previsão excluindo as 12 ultimas observações
+
+AirPassenger2 <- window(AirPassengers, start = 1949, end = c(1959,12))
+length(AirPassenger2)
+
+AirPassengersfit1 <- meanf(AirPassenger2, h = 12)
+AirPassengersfit2 <- rwf(AirPassenger2, h = 12)
+AirPassengersfit3 <- rwf(AirPassenger2, h = 12, drift = TRUE)
+AirPassengersfit4 <- snaive(AirPassenger2, h = 12)
+
+autoplot(window(AirPassengers, start = 1949)) + 
+  autolayer(AirPassengersfit1, series = "Mean", PI = FALSE) +
+  autolayer(AirPassengersfit2, series = "Naive", PI = FALSE) +
+  autolayer(AirPassengersfit3, series = "Naive com drift", PI = FALSE) +
+  autolayer(AirPassengersfit4, series = "Naive com sazonalidade", PI = FALSE) +
+  ggtitle("Previsão para o número totl de passageiros") + 
+  guides(colour = guide_legend(title = "PrevisãO"))
+  
+  
